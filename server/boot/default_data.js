df01ce80290a375b052218789e2e47e66902fe3d
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-06-04 16:16:25 -0500

    Adding deafult data in services

--- server/boot/default_data.js
+++ server/boot/default_data.js
@@ -1,18 +1,29 @@
 module.exports = function(app) {
 
-  // News
-  setTimeout(function() {
-    var data = require('../data/news.json');
-    var env = process.env.NODE_ENV;
-    var PepeNews = app.models.pepe_news;
-    if (env !== 'prod') {
-      PepeNews.create(data, function(err, userInstance) {
-        if (err) {
-          console.log(err);
-        }
-        console.log('News created successfully!');
-      });
-    }
-  }, 2000);
+  var newsData = require('../data/news.json');
+  var ServicesData = require('../data/services.json');
+  var env = process.env.NODE_ENV;
+
+  var PepeNews = app.models.pepe_news;
+  var PepeService = app.models.pepe_service;
+
+  if (env !== 'prod') {
+    PepeNews.create(newsData, function(err, userInstance) {
+      if (err) {
+        console.log(err);
+      }
+      console.log('News created successfully!');
+    });
+    PepeService.create(ServicesData, function(err, userInstance) {
+      if (err) {
+        console.log(err);
+      }
+      console.log('Services created successfully!');
+    });
+  }
+
+  // Services
+
+
 
 }
