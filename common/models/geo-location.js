e1f3178ed05c5f80cdf71754cce299ef851bffbd
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-06-04 11:39:14 -0500

    Getting lat lng from preference object with street and city

---
+++ common/models/geo-location.js
@@ -0,0 +1,37 @@
+module.exports = function(GeoLocation) {
+  // Google Maps API has a rate limit of 10 requests per second
+  // Seems we need to enforce a lower rate to prevent errors
+  var lookupGeo = require('function-rate-limit')(5, 1000, function() {
+    var geoService = GeoLocation.app.dataSources.geo;
+    geoService.geocode.apply(geoService, arguments);
+  });
+
+
+  GeoLocation.afterRemote('**', function(ctx, user, next) {
+    var body = ctx.req.body;
+    if (body &&
+      body.preferences &&
+      body.preferences.street &&
+      body.preferences.city &&
+      body.preferences.zipcode) {
+
+      console.log(body);
+      var loc = body.preferences;
+
+      // geo code the address
+      lookupGeo(loc.street, loc.city, loc.zipcode,
+        function(err, result) {
+          if (result && result[0]) {
+            body.geo = result[0];
+            next();
+          } else {
+            next(new Error('could not find location'));
+          }
+        });
+
+    } else {
+      next();
+    }
+
+  });
+};
