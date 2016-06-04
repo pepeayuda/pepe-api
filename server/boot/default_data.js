ffef4927d5d9582f470743d4ff026335eea62384
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-06-04 17:36:24 -0500

    Adding type of services and services data in the database

--- server/boot/default_data.js
+++ server/boot/default_data.js
@@ -1,11 +1,13 @@
 module.exports = function(app) {
 
   var newsData = require('../data/news.json');
+  var servicesTypeData = require('../data/serviceType.json');
   var ServicesData = require('../data/services.json');
   var env = process.env.NODE_ENV;
 
   var PepeNews = app.models.pepe_news;
-  var PepeService = app.models.pepe_service;
+  var ServicesType = app.models.servicesType;
+  var Services = app.models.service;
 
   if (env !== 'prod') {
     PepeNews.create(newsData, function(err, userInstance) {
@@ -14,7 +16,13 @@ module.exports = function(app) {
       }
       console.log('News created successfully!');
     });
-    PepeService.create(ServicesData, function(err, userInstance) {
+    ServicesType.create(servicesTypeData, function(err, userInstance) {
+      if (err) {
+        console.log(err);
+      }
+      console.log('TypeServices created successfully!');
+    });
+    Services.create(ServicesData, function(err, userInstance) {
       if (err) {
         console.log(err);
       }
